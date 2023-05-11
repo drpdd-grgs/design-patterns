3cc2965544a096e69ba98aaec7db2b5b12c3df1d
a.mukhamedzyanov <a.mukhamedzyanov@tinkoff.ru>
2023-05-11 13:02:17 +0400

    struct patterns

---
+++ bridge/App.java
@@ -0,0 +1,27 @@
+package bridge;
+
+import bridge.devices.Device;
+import bridge.devices.Radio;
+import bridge.devices.Tv;
+import bridge.remotes.AdvancedRemote;
+import bridge.remotes.BasicRemote;
+
+public class App {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
